5d5d47e6ebc53881b0a4f563ccc1d9800eaed812
alexun <alexunjm@gmail.com>
2018-04-23 21:39:29 -0500

    corrección esquema de partido

--- api/models/match.js
+++ api/models/match.js
@@ -4,7 +4,6 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var MatchSchema = Schema({
-  name: String,
   home_team: { type: Schema.ObjectId, ref: "Team" },
   away_team: { type: Schema.ObjectId, ref: "Team" },
   stage: { type: Schema.ObjectId, ref: "Stage" },
