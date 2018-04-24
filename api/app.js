da6d880c13097d08469b02a9f2c24631f76621ca
alexun <alexunjm@gmail.com>
2018-04-23 21:15:49 -0500

    definiendo las rutas de la app

--- api/app.js
+++ api/app.js
@@ -6,7 +6,9 @@ var bodyParser = require('body-parser');
 var app = express();
 
 //cargar rutas
-var user_router = require('./routes/user');
+var user_router = require("./routes/user");
+var team_router = require("./routes/team");
+var match_router = require('./routes/match');
 
 //middleware
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -23,7 +25,9 @@ app.use((req, res, next) => {
 });
 
 //rutas
-app.use('/api', user_router);
+app.use("/api", user_router);
+app.use("/api", team_router);
+app.use("/api", match_router);
 
 //exportar
 module.exports = app;
